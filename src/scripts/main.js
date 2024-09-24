689957d1d03f56d32f8f5b8a65fdcc030407ea02
juansgdev <juangomes.profissional@gmail.com>
2024-09-24 18:42:12 -0300

    Criando contexto e estrutura inicial

---
+++ src/scripts/main.js
@@ -0,0 +1,7 @@
+(() => {
+    const scene = document.getElementById("scene");
+    const context = scene.getContext("2d");
+
+    context.fillStyle = '#000';
+    context.fillRect(0,0, 100, 100);
+})();
